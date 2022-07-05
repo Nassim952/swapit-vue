ac77049bf2b969a50490126f5eae3bfaa75891c2
Andrew Mondor <mondor.andrew@gmail.com>
2022-07-05 19:32:05 +0200

    integration test get api from front

--- tests/unit/api-game.spec.js
+++ tests/unit/api-game.spec.js
@@ -1,6 +1,6 @@
 import request from "supertest";
 const baseurl =  "https://localhost/api/games.json?page=1&id=";
-describe ('testing api', () =>{
+describe ('testing api games status code and content type', () =>{
     it ('should return 200 status code',async ()=>{
         const response = await request(baseurl).get('1')
         .trustLocalhost()
@@ -9,5 +9,3 @@ describe ('testing api', () =>{
         
     });
 })
-
-
