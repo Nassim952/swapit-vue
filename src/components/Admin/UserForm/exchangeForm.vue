eae94f6f483f5a11e18cdcd124cf36c3400eeeda
nassim95 <nasfahdine@gmail.com>
2022-07-06 23:49:23 +0200

    minor fix exchange

--- src/components/Admin/UserForm/exchangeForm.vue
+++ src/components/Admin/UserForm/exchangeForm.vue
@@ -3,16 +3,16 @@
         <h2 class="title-block">Listes d'échanges</h2>
         <h4>Demandes d'échanges reçu</h4>
         <div v-if="receivedExchanges.length">
-                <div class="list-exchange" v-for="(exchange,key) in receivedExchanges" :key="key+exchange.id+114">
-                    <ExchangeCard :exchange="exchange" />
-                </div>
+            <div class="list-exchange" v-for="(exchange, key) in receivedExchanges" :key="key + exchange.id + 114">
+                <ExchangeCard :exchange="exchange" />
+            </div>
         </div>
         <div v-else>
             <p>L'utilisateur n'a reçu aucune demande d'échange</p>
         </div>
         <h4>Demandes d'échanges envoyées</h4>
         <div v-if="sentExchanges.length">
-            <div class="list-exchange" v-for="(exchange,key) in sentExchanges" :key="key+exchange.id+114">
+            <div class="list-exchange" v-for="(exchange, key) in sentExchanges" :key="key + exchange.id + 114">
                 <ExchangeCard :exchange="exchange" />
             </div>
         </div>
@@ -23,68 +23,73 @@
 </template>
 
 <script>
-    import {Exchange} from '../../../lib/Services/Exchange.js';
-    import {User} from '../../../lib/Services/User';
-    import ExchangeCard from './exchangeCard.vue';
+import { Exchange } from '../../../lib/Services/Exchange.js';
+import { User } from '../../../lib/Services/User';
+import ExchangeCard from './exchangeCard.vue';
 
 export default {
-	name:'ExchangeForm',
-    props:{
-        userId:{
-            required:true,
-            type:String
-        }
-    },
-    components:{
+    name: 'ExchangeForm',
+    components: {
         ExchangeCard
     },
-    data:function(){
-        return{
-            sentExchanges:[],
-            receivedExchanges:[],
-            receivedExchangesTmp:[],
-            sentExchangesTmp:[],
+    data: function () {
+        return {
+            sentExchanges: [],
+            receivedExchanges: [],
+            receivedExchangesTmp: [],
+            sentExchangesTmp: [],
+            user: {},
         }
     },
-    created(){
-        this.refreshExhanges();
+    created() {
+        this.getCurrentUser();
     },
-    methods:{
-        supExchange(exchange_id){
+    methods: {
+        supExchange(exchange_id) {
             const provider = new Exchange();
             provider.delExchange(exchange_id)
-            .then((response)=>{
-                if(response){
-                    this.refreshExhanges();
-                   console.log(response)
-                }
-            })
-            
+                .then((response) => {
+                    if (response) {
+                        this.refreshExhanges();
+                        console.log(response)
+                    }
+                })
+
         },
         async refreshExhanges() {
             const provider = new User();
-			provider.getReceivedExchanges(this.userId)
-			.then(response=>{
-                 console.log(response);
-                if(response){
-                    this.receivedExchanges = response;
-                }			
-			})
-			.catch(err=>{
-				console.log(err)
-			})
-            provider.getSendExchanges(this.userId)
-			.then(response=>{
-                if(response){
-                    this.sentExchanges = response;
-                }			
-			})
-			.catch(err=>{
-				console.log(err)
-			})
+            provider.getReceivedExchanges(this.$data.user.id)
+                .then(response => {
+                    console.log(response);
+                    if (response) {
+                        this.receivedExchanges = response;
+                    }
+                })
+                .catch(err => {
+                    console.log(err)
+                })
+            provider.getSendExchanges(this.$data.user.id)
+                .then(response => {
+                    if (response) {
+                        this.sentExchanges = response;
+                    }
+                })
+                .catch(err => {
+                    console.log(err)
+                })
             console.log(this.receivedExchanges)
-        }
+        },
+        async getCurrentUser() {
+            var provider = new User()
+            provider.getUser(this.$route.params.id).then(response => {
+                if (response) {
+                    this.$data.user = response
+                    this.refreshExhanges();
+                }
+            })
+        },
     },
+
     provide() {
         return {
             supExchange: this.supExchange,
@@ -95,18 +100,18 @@ export default {
 </script>
 
 <style scoped>
-.list-exchange-container{
+.list-exchange-container {
     padding: 20px;
     margin-left: 20px;
 }
 
-.list-exchange{
+.list-exchange {
     margin-top: 20px;
     display: flex;
     flex-direction: column;
 }
 
-.exchange-el{
+.exchange-el {
     margin-top: 10px;
     display: flex;
     border-radius: 20px;
@@ -114,36 +119,44 @@ export default {
     padding: 20px;
     justify-content: space-between;
 }
+
 .game_card {
-  display: flex;
-  background-color: #e2e2e2;
-  height: 5rem;
-  width: 20rem;
-  overflow: hidden;
-  border-radius: 10px;
-  font-size: 12px;
+    display: flex;
+    background-color: #e2e2e2;
+    height: 5rem;
+    width: 20rem;
+    overflow: hidden;
+    border-radius: 10px;
+    font-size: 12px;
 }
-.game_card img{
+
+.game_card img {
     width: auto;
     height: 100%;
 }
-.text-game span{
+
+.text-game span {
     color: rgb(0, 0, 0);
     padding: 10px;
 }
-.text-game{
+
+.text-game {
     padding: 10px;
 }
-.swap-img{
+
+.swap-img {
     align-self: center;
 }
-.game-info{
+
+.game-info {
     text-align: center;
 }
-.user-pseudo{
+
+.user-pseudo {
     margin-bottom: 10px;
 }
-.title-block{
+
+.title-block {
     margin-bottom: 30px;
 }
 </style>
