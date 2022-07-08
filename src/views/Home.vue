475f75bb290650d4ec9fd217c9984df3eaa942db
nassim95 <nasfahdine@gmail.com>
2022-07-08 11:46:39 +0200

    minor fix

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="row p-5 no-overflow">
         <img class="col-lg-6" src="../assets/images/swapit-landing.svg" />
-        <img class="col-lg-6 " src="../assets/images/illustration-Manette.png" />
+        <img style="width: 49%" class="col-lg-6 " src="../assets/images/illustration-Manette.png" />
     </div>
 </template>
 
