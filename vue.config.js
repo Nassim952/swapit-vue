ebd27242ff45bfa8bdc25beebf7a1620ae594e9c
nassim95 <nasfahdine@gmail.com>
2022-07-13 18:38:39 +0200

    fixing proxy

--- vue.config.js
+++ vue.config.js
@@ -4,10 +4,12 @@ module.exports = {
         proxy: {
             '/api-core': {
                 target: 'https://swapit-api-core.herokuapp.com/',
+                ws: true,
                 pathRewrite: { '^/api-core': '/' }
             },
             "/api-game": {
                 target: 'https://swapit-api-game.herokuapp.com/',
+                ws: true,
                 pathRewrite: { '^/api-game': '/' }
             }
         }
